60fc75b983c25cfe24beb5f0b1c23071e98695f4
Pranshul Pandey <pranshulpandey2211@gmail.com>
2022-10-02 22:09:19 +0530

    Optimized Bubble Sort

--- src/sorts/BubbleSort.java
+++ src/sorts/BubbleSort.java
@@ -2,14 +2,18 @@ package sorts;
 
 public class BubbleSort {
     static void sort(int arr[]){
-        for(int i= 0; i < arr.length - 1; i++){
+        for(int i = 0; i < arr.length - 1; i++){
+            boolean flag = true;
             for(int j = 0; j < arr.length - i - 1; j++){
                 if (arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag = false;
                 }
             }
+            if(flag)
+                break;
         }
     }
     public static void main(String[] args) {
