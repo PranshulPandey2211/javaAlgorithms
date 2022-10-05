e464efb8abe208aa67224bce9730fefd557abe56
Pranshul Pandey <pranshulpandey2211@gmail.com>
2022-10-05 22:46:05 +0530

    added Decimal To Octal conversion

--- src/conversions/DecimalToHexadecimal.java
+++ src/conversions/DecimalToHexadecimal.java
@@ -1,15 +1,14 @@
 package conversions;
 
-public class DecimalToHexadecimal {
+public class DecimalToHexadecimal{
     static void decToHex(int n){
-        int[] hex = new int[100];
+        int hex[] = new int[100];
         int i = 0;
         while (n != 0) {
-            hex[i] = n % 16;
+            hex[i++] = n % 16;
             n = n / 16;
-            i++;
         }
-        for (int j = i - 1; 0 <= j; j--) {
+        for (int j = i - 1; 0 <= j; j--){
             if (hex[j] > 9)
                 System.out.print((char)(55 + hex[j]));
             else
---
+++ src/conversions/DecimalToOctal.java
@@ -0,0 +1,18 @@
+package conversions;
+
+public class DecimalToOctal {
+    static void decToOct(int n){
+        int oct[] = new int[100];
+        int i = 0;
+        while (n != 0) {
+            oct[i++] = n % 8;
+            n = n / 8;
+        }
+        for (int j = i - 1; 0 <= j; j--){
+            System.out.print(oct[j]);
+        }
+    }
+    public static void main(String[] args){
+        decToOct(867503);
+    }
+}
